d8db62e9df5080f86d8e5bcb1ac4dfc01e2fe699
Sasha Vrublevskaya <Belarus>
2020-10-27 21:36:29 +0300

    .

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    let result = "";
+    let names = [];
+    
+    if (Array.isArray(members) === false) return false;
+
+    for (let i of members) {
+    if (typeof i === 'string') {
+    i = i.replace(/\s/g, '');
+    names.push(i);
+     }
+    } 
+            
+    for (let i = 0; i < names.length; i++) 
+    names[i] = names[i].toUpperCase();
+
+    names.sort();
+    
+    for (j of names) 
+    result += j.charAt(0);
+      
+    return result;
 };
