503f8c3949160e3a3a25ba10bdfdce459122758a
Sasha Vrublevskaya <Belarus>
2020-10-28 22:40:57 +0300

    .

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (options.addition === undefined) {
+    if (options.separator === undefined)
+    options.separator = "+";
+
+    let temp = options.repeatTimes - 1;
+    let tempStr = String(str);
+    while (temp !== 0)
+   {
+    str += options.separator;
+    str += tempStr;
+    temp--;
+   }      
+  }
+  else {
+    if (options.additionSeparator === undefined)
+    options.additionSeparator = "";
+    if (options.repeatTimes === undefined)
+    options.repeatTimes = 0;
+    if (options.additionRepeatTimes === undefined)
+    options.additionRepeatTimes = 0;
+      
+    let temp = options.additionRepeatTimes - 1;
+    str += String(options.addition);
+    while (temp > 0)
+    {
+      str += options.additionSeparator;
+      str += options.addition;
+      temp--;
+    }
+
+    temp = options.repeatTimes - 1;
+    let tempStr = String(str);
+    while (temp > 0)
+    {
+      str += options.separator;
+      str += tempStr;
+      temp--;
+    }
+  }
+ return str;
 };
   
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
+module.exports = function transform(/*arr*/) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  /*let expr = ""; 
+  let newArr = arr;
+  if (Array.isArray(arr) === false) 
+  throw Error("Argument is not an array!");
+
+  for (let i = 0; i < arr.length; i++) {
+  if (typeof arr[i] === 'string') {
+  expr = arr[i];
+  switch (expr) 
+      {
+        case "--discard-next":
+          newArr = arr;
+          if (arr[i+1] !== undefined) {      
+          newArr.splice(i,2);
+          }
+          else newArr.splice(i,1);
+          break;
+
+        case "--discard-prev":
+          newArr = arr;
+          if (arr[i-1] !== undefined) {      
+          newArr.splice(i-1,2);
+          }
+          else newArr.splice(i,1);
+          break;
+        
+        case "--double-next": 
+            newArr = arr;
+            if (arr[i+1] !== undefined) {
+            newArr.push(arr[i+1]);
+            newArr.splice(i,1);
+            newArr.sort(function(a, b) {return a - b;});
+            }
+            else newArr.splice(i,1);
+          break;
+        
+        case "--double-prev": 
+            newArr = arr;
+            if (arr[i-1] !== undefined) {
+            newArr.push(arr[i-1]);
+            newArr.splice(i,1);
+            newArr.sort(function(a, b) {return a - b;});
+            }
+            else newArr.splice(i,1);
+          break;        
+        default:
+      } 
+} 
+} 
+return newArr;*/
 };
